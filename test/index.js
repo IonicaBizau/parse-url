9f8aebfe550d4bca2905f0afa2d566bc7e2f218a
Ionică Bizău <bizauionica@gmail.com>
2017-06-12 09:24:04 +0300

    Handle more local paths

--- test/index.js
+++ test/index.js
@@ -103,7 +103,7 @@ const INPUTS = [
           , port: null
           , resource: ""
           , user: ""
-          , pathname: "./path/to/file.png"
+          , pathname: "path/to/file.png"
           , hash: ""
           , search: ""
         }
@@ -116,7 +116,7 @@ const INPUTS = [
           , port: null
           , resource: ""
           , user: ""
-          , pathname: "./.path/to/file.png"
+          , pathname: ".path/to/file.png"
           , hash: ""
           , search: ""
         }
