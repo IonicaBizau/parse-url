f6594d3199f4348946ff9bbcd7b95dfde4559b53
pocas <pocas@pocasui-MacBookPro.local>
2022-02-25 21:26:14 +0900

    bug fixed

--- test/index.js
+++ test/index.js
@@ -1,68 +1,136 @@
-const parser = require('../lib/index.js');
+// Dependencies
+const parseUrl = require("../lib")
+    , tester = require("tester")
+    , normalizeUrl = require("normalize-url")
+    , qs = require("querystring")
+    ;
 
-console.log("[+] Test Case - 1")
-console.log(parser("javascript://alert(1)"))
+const INPUTS = [
+    [
+        "http://ionicabizau.net/blog"
+      , {
+            protocols: [ "http" ]
+          , protocol: "http"
+          , port: null
+          , resource: "ionicabizau.net"
+          , user: ""
+          , pathname: "/blog"
+          , hash: ""
+          , search: ""
+        }
+    ]
+  , [
+        "//ionicabizau.net/foo.js"
+      , {
+            protocols: ["http"]
+          , protocol: "http"
+          , port: null
+          , resource: "ionicabizau.net"
+          , user: ""
+          , pathname: "/foo.js"
+          , hash: ""
+          , search: ""
+        }
+    ]
+  , [
+        "http://domain.com/path/name#some-hash?foo=bar"
+      , {
+            protocols: ["http"]
+          , protocol: "http"
+          , port: null
+          , resource: "domain.com"
+          , user: ""
+          , pathname: "/path/name"
+          , hash: "some-hash?foo=bar"
+          , search: ""
+        }
+    ]
+  , [
+        ["git+ssh://git@host.xz/path/name.git", false]
+      , {
+            protocols: ["git", "ssh"]
+          , protocol: "git"
+          , port: null
+          , resource: "host.xz"
+          , user: "git"
+          , pathname: "/path/name.git"
+          , hash: ""
+          , search: ""
+        }
+    ]
+  , [
+        ["git@github.com:IonicaBizau/git-stats.git", false]
+      , {
+            protocols: []
+          , protocol: "ssh"
+          , port: null
+          , resource: "github.com"
+          , user: "git"
+          , pathname: "/IonicaBizau/git-stats.git"
+          , hash: ""
+          , search: ""
+        }
+    ]
+  , [
+        ["http://ionicabizau.net/with-true-normalize", true]
+      , {
+            protocols: [ "http" ]
+          , protocol: "http"
+          , port: null
+          , resource: "ionicabizau.net"
+          , user: ""
+          , pathname: "/with-true-normalize"
+          , hash: ""
+          , search: ""
+        }
+    ]
+  , [
+        ["javascript:alert(1)", false]
+      , {
+          protocols: [ "javascript" ]
+        , protocol: "javascript"
+        , port: null
+        , resource: ""
+        , user: ""
+        , pathname: "/alert(1)"
+        , hash: ""
+        , search: ""
+    }
+  ]
+  , [
+        ["javascript://google.com/%0aalert(1)", false]
+      , {
+          protocols: [ "javascript" ]
+        , protocol: "javascript"
+        , port: null
+        , resource: "google.com"
+        , user: ""
+        , pathname: "/%0aalert(1)"
+        , hash: ""
+        , search: ""
+    }
+  ]
+];
 
-console.log("Test Case - 2")
-console.log(parser("javascript:alert(1)"))
+tester.describe("check urls", test => {
+    INPUTS.forEach(function (c) {
+        let url = Array.isArray(c[0]) ? c[0][0] : c[0]
+        test.should("support " + url, () => {
+            const res = parseUrl(url, c[0][1] !== false);
+            if (c[0][1] !== false) {
+                url = normalizeUrl(url, {
+                    stripHash: false
+                })
+            }
+            c[1].query = qs.parse(c[1].search)
+            c[1].href = url
+            test.expect(res).toEqual(c[1]);
+        });
+    });
 
-console.log("Test Case - 3")
-console.log(parser("j\x61vascript:alert(1)"))
-
-console.log("Test Case - 4")
-console.log(parser("\x00javascript:alert(1)"))
-
-/*
-[+] Test Case - 1
-{
-  protocols: [ 'javascript' ],
-  protocol: 'javascript',
-  port: null,
-  resource: 'alert(1)',
-  user: '',
-  pathname: '',
-  hash: '',
-  search: '',
-  href: 'javascript://alert(1)',
-  query: [Object: null prototype] {}
-}
-Test Case - 2
-{
-  protocols: [ 'javascript' ],
-  protocol: 'javascript',
-  port: null,
-  resource: '',
-  user: '',
-  pathname: '/alert(1)',
-  hash: '',
-  search: '',
-  href: 'javascript:alert(1)',
-  query: [Object: null prototype] {}
-}
-Test Case - 3
-{
-  protocols: [ 'javascript' ],
-  protocol: 'javascript',
-  port: null,
-  resource: '',
-  user: '',
-  pathname: '/alert(1)',
-  hash: '',
-  search: '',
-  href: 'javascript:alert(1)',
-  query: [Object: null prototype] {}
-}
-Test Case - 4
-{
-  protocols: [ 'javascript' ],
-  protocol: 'javascript',
-  port: null,
-  resource: '',
-  user: '',
-  pathname: '/alert(1)',
-  hash: '',
-  search: '',
-  href: 'javascript:alert(1)',
-  query: [Object: null prototype] {}
-}
-*/
+    test.should("throw if url is empty", () => {
+        test.expect(() => {
+            parseUrl("")
+        }).toThrow(/invalid url/i)
+    })
+});
---
+++ test/remove_white_space.js
@@ -0,0 +1,2 @@
+const parser = require('../lib/index.js');
+console.log(parser("\x00javascript:alert(1)"))
