c6e6991327a9eb6653bf3205dfcabc4974156183
Ionică Bizău <bizauionica@gmail.com>
2022-06-27 12:33:14 +0300

    :arrow_up: 7.0.1 :tada:

--- example/index.js
+++ example/index.js
@@ -2,46 +2,62 @@
 const parseUrl = require("../lib")
 
 console.log(parseUrl("http://ionicabizau.net/blog"))
-// { protocols: [ 'http' ],
+// {
+//   protocols: [ 'http' ],
 //   protocol: 'http',
-//   port: null,
+//   port: '',
 //   resource: 'ionicabizau.net',
 //   user: '',
+//   password: '',
 //   pathname: '/blog',
 //   hash: '',
 //   search: '',
-//   href: 'http://ionicabizau.net/blog' }
+//   href: 'http://ionicabizau.net/blog',
+//   query: {}
+// }
 
 console.log(parseUrl("http://domain.com/path/name?foo=bar&bar=42#some-hash"))
-// { protocols: [ 'http' ],
+// {
+//   protocols: [ 'http' ],
 //   protocol: 'http',
-//   port: null,
+//   port: '',
 //   resource: 'domain.com',
 //   user: '',
+//   password: '',
 //   pathname: '/path/name',
 //   hash: 'some-hash',
 //   search: 'foo=bar&bar=42',
-//   href: 'http://domain.com/path/name?foo=bar&bar=42#some-hash' }
+//   href: 'http://domain.com/path/name?foo=bar&bar=42#some-hash',
+//   query: { foo: 'bar', bar: '42' }
+// }
 
 // If you want to parse fancy Git urls, turn off the automatic url normalization
 console.log(parseUrl("git+ssh://git@host.xz/path/name.git", false))
-// { protocols: [ 'git', 'ssh' ],
+// {
+//   protocols: [ 'git', 'ssh' ],
 //   protocol: 'git',
-//   port: null,
+//   port: '',
 //   resource: 'host.xz',
 //   user: 'git',
+//   password: '',
 //   pathname: '/path/name.git',
 //   hash: '',
 //   search: '',
-//   href: 'git+ssh://git@host.xz/path/name.git' }
+//   href: 'git+ssh://git@host.xz/path/name.git',
+//   query: {}
+// }
 
 console.log(parseUrl("git@github.com:IonicaBizau/git-stats.git", false))
-// { protocols: [],
+// {
+//   protocols: [ 'ssh' ],
 //   protocol: 'ssh',
-//   port: null,
+//   port: '',
 //   resource: 'github.com',
 //   user: 'git',
+//   password: '',
 //   pathname: '/IonicaBizau/git-stats.git',
 //   hash: '',
 //   search: '',
-//   href: 'git@github.com:IonicaBizau/git-stats.git' }
+//   href: 'git@github.com:IonicaBizau/git-stats.git',
+//   query: {}
+// }
